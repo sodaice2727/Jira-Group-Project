be26edc3255767001971f84f30966e83c429ab58
Jiranuwat Songpad <jiranuwat.song64@gmail.com>
2025-05-22 14:33:43 +0700

    JGP-6 done

--- src/App.tsx
+++ src/App.tsx
@@ -22,19 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR แก้แล้วจ้า
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-
-
-      <p>
-        Test Login by Patthama
-
-      <p>
-
       </p>
     </>
   )
